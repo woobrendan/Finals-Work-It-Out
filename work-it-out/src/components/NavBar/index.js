989f3a13e85baca121421551520ce53d99192758
Brendan Woo <woo.brendan@gmail.com>
2022-03-27 16:20:54 -0400

    add two menus to nav bar with dropdown options for navigation. add css

--- work-it-out/src/components/NavBar/index.js
+++ work-it-out/src/components/NavBar/index.js
@@ -1,6 +1,12 @@
 import React, {useContext, useState} from "react";
 import "./NavBar.css";
 import {Link} from "react-router-dom";
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import {useNavigate } from 'react-router-dom'
+import ExerciseMenu from "./Exercise_menu";
+import NavMenu from "./Nav_menu";
 
 // Importing material UI icons
 import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
@@ -9,6 +15,16 @@ import { UserContext } from "../../helpers/UserContext";
 export default function NavBar() {
   const {user} = useContext(UserContext);
   const [showAuth, setShowAuth] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const navigate = useNavigate();
+
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
 
   const linkStyle = {
@@ -24,42 +40,46 @@ export default function NavBar() {
             <h1 className="brand">Work <span className="dumbbell" >  I</span>t Out </h1>
           </Link>
         </div>
-        <div className="menu">
-          {user.name && <div>Signed in as:<br></br> {user.name}</div>}
-          <div onClick={() => setShowAuth(!showAuth)} style={linkStyle}>
-            <PersonRoundedIcon sx={{fontSize: 50}} />
-          </div>
-          {showAuth && <div className="menu-options">
-            {!user.name && (
-              <>
-                <div>
-                  <span>
-                    <Link to="/login" style={linkStyle}>Login</Link>
-                  </span>
-                </div>
-                <div>
-                  <span>
-                    <Link to="/register" style={linkStyle}>Register</Link>
-                  </span>
-                </div>
-              </>
-            )}
-            {user.name && (
-              <>
-                <div>
-                  <span>
-                    <Link to="/profile" style={linkStyle}>Profile</Link>
-                  </span>
-                </div>
-                <div>
-                  <span>
-                    <div onClick={() => console.log("click")} style={linkStyle}>Logout</div>
-                  </span>
-                </div>
-              </>
-            )}
-          </div>}
-        </div>  
+        <div className="Nav-top-right-container">
+          <ExerciseMenu />
+          <NavMenu />    
+          <div className="menu">
+            {user.name && <div>Signed in as:<br></br> {user.name}</div>}
+            <div onClick={() => setShowAuth(!showAuth)} style={linkStyle}>
+              <PersonRoundedIcon sx={{fontSize: 50}} />
+            </div>
+            {showAuth && <div className="menu-options">
+              {!user.name && (
+                <>
+                  <div>
+                    <span>
+                      <Link to="/login" style={linkStyle}>Login</Link>
+                    </span>
+                  </div>
+                  <div>
+                    <span>
+                      <Link to="/register" style={linkStyle}>Register</Link>
+                    </span>
+                  </div>
+                </>
+              )}
+              {user.name && (
+                <>
+                  <div>
+                    <span>
+                      <Link to="/profile" style={linkStyle}>Profile</Link>
+                    </span>
+                  </div>
+                  <div>
+                    <span>
+                      <div onClick={() => console.log("click")} style={linkStyle}>Logout</div>
+                    </span>
+                  </div>
+                </>
+              )}
+            </div>}
+          </div>  
+        </div>
       </div>
     </nav>
   );
